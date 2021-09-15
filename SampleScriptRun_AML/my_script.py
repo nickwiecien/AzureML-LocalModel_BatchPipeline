a9fa71826b8a40172352ff6ae119da9f7cc90b22
Nick Kwiecien <Nick.Kwiecien@microsoft.com>
2021-09-15 11:53:26 -0500

    Updated my_script.py to demonstrate workspace access in submitted run

--- SampleScriptRun_AML/my_script.py
+++ SampleScriptRun_AML/my_script.py
@@ -1,6 +1,14 @@
-#Import required packages
+from azureml.core import Run, Workspace, Datastore, Dataset
 import pandas as pd
 import snowflake.connector
 
-#Execute logic below
 print('hello world')
+
+#Get current run
+current_run = Run.get_context()
+
+#Get associated AML workspace
+ws = current_run.experiment.workspace
+
+#Print workspace details
+print(ws)
